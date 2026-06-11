29e9cd989aefc6cabf86a794ed041d91621fa1f9
photoacchp-crypto <photoacchp@gmail.com>
2026-06-11 10:21:56 +0700

    Add configuration for Creator Vault app

---
+++ js/config.js
@@ -0,0 +1,6 @@
+// Google Apps Script API URL
+const API_URL = "https://script.google.com/macros/s/AKfycbxnp6QDle7-7XXQqn1s5vJvyKpJeEILEb5soYn64tHtmzhMERiG7DZ_AivkLH9vgeA3/exec";
+// App configuration
+const APP_NAME = "Creator Vault";
+const APP_VERSION = "2.0";
+const ITEMS_PER_PAGE = 20;
