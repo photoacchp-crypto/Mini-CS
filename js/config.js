38cf52471bd33f7dc49975fff0df899fd9ab014f
photoacchp-crypto <photoacchp@gmail.com>
2026-06-11 10:27:11 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 // Google Apps Script API URL
-const API_URL = "https://script.google.com/macros/s/AKfycbxnp6QDle7-7XXQqn1s5vJvyKpJeEILEb5soYn64tHtmzhMERiG7DZ_AivkLH9vgeA3/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbyfDII52VgwAARpw0xsYg9ArPZo5O88Wg4taCdlHWgYvOOzzLv58wS4uQzXkw3eftT1/exec";
 // App configuration
 const APP_NAME = "Creator Vault";
 const APP_VERSION = "2.0";
