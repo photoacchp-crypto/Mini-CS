24af7473c168fbb92d80d1864add37c7ff2bcd4c
photoacchp-crypto <photoacchp@gmail.com>
2026-06-11 10:40:55 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
-// Google Apps Script API URL
+// Google Apps Script API URL - SOURCE DATABASE BARU
 const API_URL = "https://script.google.com/macros/s/AKfycbymziyZ0ZS0syO0c8szOsE_nwCzQtTU07JlPFat0BYDey-OxiuqeSlyLvYlXqZ4gvdv/exec";
 
 // App configuration
